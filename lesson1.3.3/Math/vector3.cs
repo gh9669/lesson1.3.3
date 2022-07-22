7e9ce7c8883ccce367398a7f9e2ed38d14216610
Flvby <pupok.v.66@gmail.com>
2022-07-22 14:56:51 +0300

    Добавьте файлы проекта.

---
+++ lesson1.3.3/Math/vector3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson1._3._3.Math
+{
+    public struct vector3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public vector3 add(vector3 other)
+        {
+            vector3 result = new vector3()
+            {
+                X = X + other.X,
+                Y = Y + other.Y,
+                Z = Z + other.Z,
+            };
+            return result;
+        }
+
+        public vector3 Subtract (vector3 other)
+        {
+            vector3 result = new vector3()
+            {
+                X = X - other.X,
+                Y = Y - other.Y,
+                Z = Z - other.Z,
+            };
+            return result;
+        }
+
+        public vector3 Multiply(float number)
+        {
+            vector3 result = new vector3()
+            {
+                X = X * number,
+                Y = Y * number,
+                Z = Z * number,
+            };
+            return result;
+        }
+
+        public vector3 Divide(float number)
+        {
+            vector3 result = new vector3()
+            {
+                X = X / number,
+                Y = Y / number,
+                Z = Z / number,
+            };
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + "; " + Y + "; " + Z + ")";
+        }
+
+    }
+}
---
+++ lesson1.3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using lesson1._3._3.Math;
+
+namespace lesson1._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vector3 up = new vector3();
+            up.Y = 1;
+            vector3 forward = new vector3();
+            forward.Z = 1;
+
+            Console.WriteLine(up.add(forward));
+            Console.WriteLine(up.Subtract(forward));
+            Console.WriteLine(up.Multiply(5.5f));
+            Console.WriteLine(up.Divide(8.0f));
+        }
+    }
+}
